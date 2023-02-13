29f6fada6ea7c4c37b8cab2fd1f8c709fe13e89b
91704 <91704@Abhishek>
2023-02-13 12:47:34 +0530

    First commit

---
+++ chebeloproject/src/test/java/chebeloproject/Instalogin.java
@@ -0,0 +1,29 @@
+package chebeloproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Instalogin {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://edu-adminweb.adaptit.com/");
+		
+		//WebElement login = driver.findElement(By.xpath("//span[text()=' Sign In']"));
+		//login.click();
+		driver.findElement(By.id("emailInput")).sendKeys("9910");
+		driver.findElement(By.id("passwordInput")).sendKeys("1234");
+		WebElement login = driver.findElement(By.xpath("//span[text()=' Sign In']"));
+		login.click();
+		
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
